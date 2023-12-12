a0e9fd2d52172190ecc132263e0ef5c171b54f75
mikesae <msaelens@gmail.com>
2023-12-12 15:16:54 -0500

    Fix layout and update app title.

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,44 @@
-import { Flex, Heading, View } from "@aws-amplify/ui-react";
-import maplibregl from "maplibre-gl";
-import "maplibre-gl/dist/maplibre-gl.css";
-import React, { useEffect, useState } from "react";
-import "./App.scss";
-import MapLegend from "./MapLegend";
-import { initializeMap } from "./mapping";
+import { Flex, Heading, View } from '@aws-amplify/ui-react';
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import React, { useEffect, useState } from 'react';
+import './App.scss';
+import MapLegend from './MapLegend';
+import { initializeMap } from './mapping';
 
 function App() {
-    const [map, setMap] = useState<maplibregl.Map | null | any>(null);
+  const [map, setMap] = useState<maplibregl.Map | null | any>(null);
 
-    useEffect(() => {
-        async function initMap() {
-            setMap(await initializeMap());
-        }
-        if (map === null) {
-            initMap();
-        }
-        // Clean up on unmount
-        return () => map?.remove();
-    }, [map]);
+  useEffect(() => {
+    async function initMap() {
+      setMap(await initializeMap());
+    }
+    if (map === null) {
+      initMap();
+    }
+    // Clean up on unmount
+    return () => map?.remove();
+  }, [map]);
 
-    return (
-        <>
-            <Flex
-                placeholder='top'
-                className='top-navbar'
-                direction='row'
-                justifyContent='flex-start'
-                alignItems='stretch'
-                alignContent='flex-start'
-                wrap='nowrap'
-                gap='1rem'>
-                <Heading placeholder='heading' level={5}>MapHead v2</Heading>
-            </Flex>
-            {map && <MapLegend map={map}></MapLegend>}
-            <View className='map' id='map'></View>
-        </>
-    );
+  return (
+    <>
+      <Flex
+        placeholder='top'
+        className='top-navbar'
+        direction='row'
+        justifyContent='flex-start'
+        alignItems='stretch'
+        alignContent='flex-start'
+        wrap='nowrap'
+      >
+        <Heading placeholder='heading' level={5}>
+          MapHead v2
+        </Heading>
+      </Flex>
+      <View className='map' id='map'></View>
+      {map && <MapLegend map={map}></MapLegend>}
+    </>
+  );
 }
 
 export default App;
--- src/LayerLegendItem.tsx
+++ src/LayerLegendItem.tsx
@@ -1,56 +1,57 @@
-import { CheckboxField, Flex } from "@aws-amplify/ui-react";
-import React, { FunctionComponent } from "react";
+import { CheckboxField, Flex } from '@aws-amplify/ui-react';
+import React, { FunctionComponent } from 'react';
 
 interface Props {
-    label: string;
-    onChange?: (checked: boolean) => void;
-    imageSource?: string;
-    checked?: boolean;
-    disabled?: boolean;
+  label: string;
+  onChange?: (checked: boolean) => void;
+  imageSource?: string;
+  checked?: boolean;
+  disabled?: boolean;
 }
 
 const LayerLegendItem: FunctionComponent<Props> = ({ label, onChange, imageSource, checked, disabled }) => {
-    function makeLabel(text: string, imageSource?: string) {
-        return (
-            <>
-                {imageSource && (
-                    <img
-                        style={{ position: "relative", top: "4px", marginRight: "0.5em" }}
-                        src={imageSource}
-                        height='24'
-                        width='24'
-                        alt=''
-                    />
-                )}
-                {text}
-            </>
-        );
-    }
-
+  function makeLabel(text: string, imageSource?: string) {
     return (
-        <Flex
-            placeholder='row'
-            direction='row'
-            justifyContent='flex-start'
-            alignItems='stretch'
-            alignContent='flex-start'
-            wrap='nowrap'
-            gap='1rem'>
-            <CheckboxField
-                style={{ display: "inline-block" }}
-                onChange={(e) => {
-                    if (onChange) {
-                        onChange(e.target.checked);
-                    }
-                }}
-                label={makeLabel(label, imageSource)}
-                name={""}
-                value={""}
-                checked={checked}
-                disabled={disabled}
-                crossOrigin=''></CheckboxField>
-        </Flex>
+      <>
+        {imageSource && (
+          <img
+            style={{ position: 'relative', top: '4px', marginRight: '0.5em' }}
+            src={imageSource}
+            height='24'
+            width='24'
+            alt=''
+          />
+        )}
+        {text}
+      </>
     );
+  }
+
+  return (
+    <Flex
+      placeholder='row'
+      direction='row'
+      justifyContent='flex-start'
+      alignItems='stretch'
+      alignContent='flex-start'
+      wrap='nowrap'
+      gap='1rem'
+    >
+      <CheckboxField
+        onChange={(e) => {
+          if (onChange) {
+            onChange(e.target.checked);
+          }
+        }}
+        label={makeLabel(label, imageSource)}
+        name=''
+        value=''
+        checked={checked}
+        disabled={disabled}
+        crossOrigin=''
+      ></CheckboxField>
+    </Flex>
+  );
 };
 
 export default LayerLegendItem;
